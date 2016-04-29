1d17c339343622c27cf9f3e0b5f669e8764898bb
Kalana Gamlath <Kalana.Gamlath@nicta.com.au>
2016-04-29 11:18:36 +1000

    Updated qemu debug script to use non-legacy serial redirection.

--- qemu_debug.sh
+++ qemu_debug.sh
@@ -1,2 +1,3 @@
-qemu-system-i386 -nographic  -m 512 -kernel images/kernel-ia32-pc99   -initrd images/capdl-loader-experimental-image-ia32-pc99 \
--serial telnet:127.0.0.1:1234,server,nowait
+qemu-system-i386 -nographic -m 512 -kernel images/kernel-ia32-pc99   -initrd images/capdl-loader-experimental-image-ia32-pc99 \
+-chardev socket,host=127.0.0.1,port=1234,id=gnc0,server,nowait \
+-device isa-serial,chardev=gnc0 \
